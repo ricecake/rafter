43ae4601967a0d77b2b8a9218d98f48eb0a5510b
Leonhard Markert <curiousleo@ymail.com>
2013-12-01 17:54:55 +0000

    vote function

--- voting.erl
+++ voting.erl
@@ -1,6 +1,6 @@
 -module(voting).
 
--export([init_vstate/1]).
+-export([init_vstate/1, vote/3]).
 
 -include("voting.hrl").
 
@@ -20,3 +20,28 @@ init_vstate_tree(Parent,
     States = lists:map(fun(Child) -> init_vstate_tree(Struct, Child) end,
                        Structs),
     #vstate_v{parent = Parent, children = States, thresh = T}.
+
+-spec vote(#vstate_v{} | #vstate_p{}, [ non_neg_integer ], yes | no) ->
+    #vstate_v{} | accept | reject.
+
+vote(State = #vstate_v{children = States, yes_votes = YesVotes, thresh = T,
+                       no_votes = NoVotes},
+     [Index|Path], Vote) ->
+    {Init, [Node1|Tail]} = lists:split(Index, States),
+    case vote(Node1, Path, Vote) of
+        accept ->
+            case YesVotes + 1 >= T of
+                true -> accept;
+                false -> State#vstate_v{yes_votes = YesVotes + 1}
+            end;
+        reject ->
+            case NoVotes + 1 > length(States) - T of
+                true -> reject;
+                false -> State#vstate_v{no_votes = NoVotes + 1}
+            end;
+        Node2 ->
+            State#vstate_v{children = Init ++ [Node2|Tail]}
+    end;
+
+vote(#vstate_p{vote = pending}, [], yes) -> accept;
+vote(#vstate_p{vote = pending}, [], no) -> reject.

ca3a48ad24cc9c923a2413cbeadb35002b23b70b
Leonhard Markert <curiousleo@ymail.com>
2013-12-01 15:43:01 +0000

    whitespace

--- voting.erl
+++ voting.erl
@@ -6,11 +6,11 @@
 
 -spec init_vstate(#vstruct_v{}, #vstruct_p{} | #vstruct_v{}) ->
     #vstate_p{} | #vstate_v{}.
-%% Leaf vstruct
+
 init_vstate(Parent, #vstruct_p{}) ->
     #vstate_p{parent = Parent};
+
 init_vstate(Parent, Struct = #vstruct_v{thresh = T, children = Structs}) ->
-    States = lists:map(
-                   fun(Child) -> init_vstate(Struct, Child) end,
-                   Structs),
+    States = lists:map(fun(Child) -> init_vstate(Struct, Child) end,
+                       Structs),
     #vstate_v{parent = Parent, children = States, thresh = T}.

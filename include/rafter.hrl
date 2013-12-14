58b67f86722cce83fa82156a7bd6be89f2a12538
Leonhard Markert <curiousleo@ymail.com>
2013-12-14 17:17:46 +0000

    added voting structure and state types and records

--- include/rafter.hrl
+++ include/rafter.hrl
@@ -1,5 +1,46 @@
 -type peer() :: atom() | {atom(), atom()}.
 
+%% Voting structure types & records
+-type path() :: [ non_neg_integer() ].
+
+-type index() :: { peer(), [ path() ] }.
+
+-type vote() :: pending | yes | no.
+
+-record(vstruct_p, {
+          votes = 1 :: non_neg_integer(),
+          id :: peer()
+}).
+
+-record(vstruct_v, {
+          votes = 1 :: non_neg_integer(),
+          thresh :: non_neg_integer(),
+          children :: [ #vstruct_v{} | #vstruct_p{} ]
+}).
+
+-record(vstruct, {
+          tree :: #vstruct_v{},
+          indices :: [ index() ]
+}).
+
+-record(vstate_p, {
+          votes = 1 :: non_neg_integer(),
+          vote = pending :: vote()
+}).
+
+-record(vstate_v, {
+          votes = 1 :: non_neg_integer(),
+          yes_votes = 0 :: non_neg_integer(),
+          no_votes = 0 :: non_neg_integer(),
+          thresh :: non_neg_integer(),
+          children :: [ #vstate_v{} | #vstate_p{} ]
+}).
+
+-record(vstate, {
+          tree :: #vstate_v{},
+          indices :: [ index() ]
+}).
+
 %% Transport Independent MESSAGES
 -record(request_vote, {
             term :: non_neg_integer(),
@@ -65,7 +106,7 @@
         %% any majority of oldservers and any majority of the newservers.
         transitional,
 
-    oldservers = [] :: list(),
-    newservers = [] :: list()
+    oldvstruct :: #vstruct{},
+    newvstruct :: #vstruct{}
 }).
 

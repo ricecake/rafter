22dd1a4162f599474c2391ca16a962d925fd7b98
Leonhard Markert <curiousleo@ymail.com>
2014-01-07 11:17:32 +0100

    move logdir to /tmp to avoid hard disk writes

--- test/rafter_system_test_eqc.erl
+++ test/rafter_system_test_eqc.erl
@@ -36,7 +36,7 @@
     eqc:on_output(fun(Str, Args) ->
                 io:format(user, Str, Args) end, P)).
 
--define(logdir, "./rafter_logs").
+-define(logdir, "/tmp/rafter_logs").
 
 %% ====================================================================
 %% Tests
